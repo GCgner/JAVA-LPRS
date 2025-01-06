f24715d79cc9e5340ad68659a3c3fccdf42911f4
Alex <a.thomas@lprs.fr>
2025-01-06 14:10:19 +0100

    Ajout du projet

---
+++ LPRS/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.lprs {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.lprs to javafx.fxml;
+    exports com.example.lprs;
+}
